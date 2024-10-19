ee32a8999bfd229d53d72d5ef274ac685c087472
ahkhan13 <khanahs3351@gmail.com>
2024-10-19 17:22:49 +0530

    front _end url set in origin

--- back_end/server.js
+++ back_end/server.js
@@ -12,7 +12,7 @@ dotenv.config({
 const app=express()
 app.use(
     cors({
-      origin:['http://localhost:5173',], 
+      origin:['http://localhost:5173',"https://nimesh-portfolio-c40y.onrender.com"], 
       credentials: true, // Allow cookies and credentials
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow these methods
       allowedHeaders: ['Content-Type', 'Authorization'], // Ensure headers are allowed
